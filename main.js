31009f85a978b09f315b8db6ca289e918a176d04
StartTodayTattoo <75451692+StartTodayTattoo@users.noreply.github.com>
2020-12-03 21:29:25 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+/* javascript code for the scroll to top button: */
+//Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/*intercepting the javascript element that causes the page to regresh when the form button is pressed*/
+document.querySelector("#myButton").addEventListener("click", function (event) {
+  event.preventDefault();
+});
